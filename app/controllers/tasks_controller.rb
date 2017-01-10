17ed261ff017f3d83cdf78f76ba081d2db1baedf
lth08091998 <lth08091998@gmail.com>
2017-01-10 09:15:37 +0700

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,67 +1,65 @@
 class TasksController < ApplicationController 
-  def index
-	@q = Task.ransack(params[:q])
-	@tasks = @q.result(distinct: true)
-	@tasks.each do|task|
-		day = Time.now - Time.parse(task.title)
-		# I send email to remind one hour before the task
-		if  (((day / 3600).to_f >= -1) && ((day / 3600).to_f < 0)) 
-			ExampleMailer.sample_email(task).deliver
+	def index
+		@q = Task.ransack(params[:q])
+		@tasks = @q.result(distinct: true)
+		@tasks.each do|task|
+			day = Time.now - Time.parse(task.title)
+			# I send email to remind one hour before the task
+			if  (((day / 3600).to_f >= -1) && ((day / 3600).to_f < 0)) 
+				ExampleMailer.sample_email(task).deliver
+			end
+			task.title = Time.parse(task.title)
 		end
-		task.title = Time.parse(task.title)
+		@tasks = @tasks.sort_by { |a| [a.completed ? 1 : 0, a.title] }
 	end
-	@tasks = @tasks.sort { |a,b| b[:title] <=> a[:title] }
-	@tasks = @tasks.sort_by { |a| a.completed ? 1 : 0 }
-  end
 
-  def complete
-   @task = Task.find(params[:id])
-   @task.completed = true
-   @task.save
-   redirect_to tasks_path
-  end
+	def complete
+		@task = Task.find(params[:id])
+		@task.completed = true
+		@task.save
+	    redirect_to tasks_path
+    end
 	
-  def show
-    @task = Task.find(params[:id])
-  end
- 
-  def edit
-     @task = Task.find(params[:id])
-  end
-  
-  def new
-	@task = Task.new
-  end
- 
-  def create
-	@task = Task.new(task_params)
-	@task.completed = false
-	if @task.save
-		redirect_to @task
-	else
-		render 'new'
+	def show
+		@task = Task.find(params[:id])
 	end
-  end
  
-def update
-  @task = Task.find(params[:id])
+	def edit
+		@task = Task.find(params[:id])
+	end
+  
+	def new
+		@task = Task.new
+	end
  
-  if @task.update(task_params)
-    redirect_to @task
-  else
-    render 'edit'
-  end
-end
+	def create
+		@task = Task.new(task_params)
+		@task.completed = false
+		if @task.save
+			redirect_to @task
+		else
+			render 'new'
+		end
+	end
  
- def destroy
-  @task = Task.find(params[:id])
-  @task.destroy
+	def update
+		@task = Task.find(params[:id])
  
-  redirect_to tasks_path
-end
+		if @task.update(task_params)
+			redirect_to @task
+		else
+			render 'edit'
+		end
+	end
+ 
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy 
+		redirect_to tasks_path
+	end
 
-private
-  def task_params
-    params.require(:task).permit(:title, :text, :tag_list)
-  end
+	private
+	def task_params
+		params.require(:task).permit(:title, :text, :tag_list)
+	end
 end
